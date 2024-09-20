347d71dbfc7a8c2adba30f94c4eac85955d8955d
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-20 09:54:39 +0000

    Added ready section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import BannerSection from "@/components/BannerSection/BannerSection";
 import Navbar from "@/components/Navbar/Navbar";
+import ReadySection from "@/components/ReadySection/ReadySection";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Navbar></Navbar>
       <BannerSection></BannerSection>
+      <ReadySection></ReadySection>
     </div>
   );
 }
---
+++ src/components/ReadySection/ReadySection.tsx
@@ -0,0 +1,10 @@
+function ReadySection() {
+    return (
+        <div className="container mx-auto text-white mt-10 text-center">
+            <p className="uppercase text-[8rem] font-bold">Be Ready</p>
+            <p className="uppercase text-[8rem] -mt-16 font-bold">Be Better</p>
+        </div>
+    )
+}
+
+export default ReadySection;
