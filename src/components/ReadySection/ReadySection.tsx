b0976dc85c53cebbdf266903ba2bf99dabcfcf4b
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-20 09:59:52 +0000

    Made ready section responsive

--- src/components/ReadySection/ReadySection.tsx
+++ src/components/ReadySection/ReadySection.tsx
@@ -1,8 +1,8 @@
 function ReadySection() {
     return (
         <div className="container mx-auto text-white mt-10 text-center">
-            <p className="uppercase text-[8rem] font-bold">Be Ready</p>
-            <p className="uppercase text-[8rem] -mt-16 font-bold">Be Better</p>
+            <p className="uppercase md:text-7xl text-4xl lg:text-[8rem] font-bold">Be Ready</p>
+            <p className="uppercase md:text-7xl text-4xl lg:text-[8rem] font-bold">Be Better</p>
         </div>
     )
 }
