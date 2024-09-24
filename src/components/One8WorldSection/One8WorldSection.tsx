0fe10bfdef3f87c91427292311f6ddbc99d976ba
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-24 12:38:08 +0000

    Adding one8world section

--- src/components/One8WorldSection/One8WorldSection.tsx
+++ src/components/One8WorldSection/One8WorldSection.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 const One8WorldSection: React.FC = () => {
     return (
         <section className="flex items-center justify-center h-screen">
-            <div className="text-center">
+            <div>
                 {/* one8 text */}
-                <h1 className="text-6xl md:text-8xl font-bold text-yellow-500">one8</h1>
+                <h1 className="lg:text-[14rem] text-6xl md:text-8xl text-yellow-500">one8</h1>
 
                 {/* WORLD text */}
-                <h2 className="text-6xl md:text-8xl font-bold text-transparent mt-4 tracking-wide">
-                    <span className="border-4 border-yellow-500 px-2 py-1">
-                        WORLD
-                    </span>
+                <h2 className="lg:text-[12rem] md:text-7xl text-4xl font-bold text-transparent outline-yellow">
+                    WORLD
                 </h2>
             </div>
         </section>
