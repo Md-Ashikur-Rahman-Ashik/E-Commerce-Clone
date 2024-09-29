c2215b1f6a9959d23098409e08fc22e898bbb4a9
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-29 09:07:48 +0000

    Added first section of second page

--- src/app/who-are-we/page.tsx
+++ src/app/who-are-we/page.tsx
@@ -5,22 +5,22 @@ const WhoAreWe = () => {
     return (
         <>
             <Navbar></Navbar>
-            <div className="relative h-screen w-full text-white flex">
+            <div className="relative container mx-auto h-screen w-full text-white flex">
                 {/* Background Image */}
                 <div className="absolute inset-0 bg-cover bg-center bg-no-repeat opacity-50" style={{ backgroundImage: 'url(/path-to-your-image.jpg)' }}></div>
 
                 {/* Text Container */}
-                <div className="relative z-10 top-40 text-center">
+                <div className="relative z-10 w-full top-40">
                     {/* "BE READY" Text */}
                     <h1 className="text-yellow-500 font-bold text-8xl lg:text-[10rem] md:text-6xl sm:text-4xl">BE READY</h1>
 
                     {/* "BE BETTER" Text with Outline */}
-                    <h1 className="text-transparent font-bold text-8xl lg:text-[10rem] md:text-6xl sm:text-4xl outline-white2" style={{ WebkitTextStroke: '2px white' }}>
+                    <h1 className="text-transparent text-right font-bold text-8xl lg:text-[10rem] md:text-6xl sm:text-4xl outline-white2" style={{ WebkitTextStroke: '2px white' }}>
                         BE BETTER
                     </h1>
 
                     {/* Paragraph Section */}
-                    <p className="text-lg mt-8 max-w-2xl mx-auto sm:text-sm">
+                    <p className="lg:text-xl mt-8 max-w-2xl sm:text-sm">
                         What started as a shirt number on the back of a young talent, became a phenomenon in the sporting world and an inspiration for billions in a global community that dares to express its unique sense of style.
                     </p>
                 </div>
